739153a3708616e1c8721febc2ace95ffddeb6ef
Maxim Koshevoy <bocchitheprog@gmail.com>
2023-09-10 15:19:17 +0300

    Added additional checks

--- SimpleGame.rb
+++ SimpleGame.rb
@@ -1,29 +1,32 @@
-if ARGV[0] == ARGV[1]
-  puts "Draw"
-elsif ARGV[0] == "Stone"
-  if ARGV[1] == "Paper"
-    puts "You loose"
-  else 
-    puts "You win"
-  end
-elsif ARGV[0] == "Paper"
-  if ARGV[1] == "Scissors"
-    puts "You loose"
-  else 
-    puts "You win"
-  end
-elsif ARGV[0] == "Scissors"
-  if ARGV[1] == "Stone"
-    puts "You loose"
-  else 
-    puts "You win"
-  end
-else 
-  puts "Impossible choice"
-end
-#puts("Hello Ruby")
+#possible options to play
+options = ["Stone", "Paper", "Scissors"]
 
-#puts "Input arguments:"
-#for element in ARGV
-#  puts element
-#end
+if ARGV.length != 2
+  puts "You need two players"
+elsif options.include? ARGV[0] and options.include? ARGV[1]
+  if ARGV[0] == ARGV[1]
+    puts "Tie"
+  elsif ARGV[0] == "Stone"
+    if ARGV[1] == "Paper"
+      puts "You loose"
+    else 
+      puts "You win"
+    end
+  elsif ARGV[0] == "Paper"
+    if ARGV[1] == "Scissors"
+      puts "You loose"
+    else 
+      puts "You win"
+    end
+  elsif ARGV[1] == "Stone"
+    puts "You loose"
+  else 
+    puts "You win"
+  end
+else
+  puts "Impossible choice, you can choose between:";
+  for element in options
+    puts element
+  end
+end
+
